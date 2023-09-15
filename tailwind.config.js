5051d43fb6b5ec3fd98a85a757da9128400964f0
Wizardjeremie <jeremiewebpro@gmail.com>
2023-09-15 13:05:48 +0200

    Learned how to config tailwind and how to add borders

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
